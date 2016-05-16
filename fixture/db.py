558cd5f6fb8459a891340f304a70acb56ceb298c
Manolaru <manolaru@yahoo.co.uk>
2016-05-16 16:17:30 +0300

    Loading data

---
+++ fixture/db.py
@@ -0,0 +1,68 @@
+
+import mysql.connector
+from model.group import Group
+from model.user import User
+
+class DbFixture:
+
+   def __init__(self, host, name, user, password):
+       self.host = host
+       self.name = name
+       self.user = user
+       self.password = password
+       self.connection = mysql.connector.connect (host=host, database=name, user=user, password=password)
+       self.connection.autocommit = True
+
+
+   def get_group_list(self):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer)= row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+
+   def get_group_list_sort(self):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list order by group_name")
+            for row in cursor:
+                (id, name, header, footer)= row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+
+   def get_user_list(self):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname)= row
+                list.append(User(id=str(id), fname=firstname, lname=lastname))
+        finally:
+            cursor.close()
+        return list
+
+   def destroy(self):
+        self.connection.close()
+
+
+   def get_groups_by_user_id(self, user_id):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id from address_in_groups where id = "+user_id)
+            for row in cursor:
+                list.append(row[0])
+        finally:
+            cursor.close()
+        return list
