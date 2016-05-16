73571447e8ec0f68695f04623675140aeffabed8
Manolaru <manolaru@yahoo.co.uk>
2016-05-16 22:35:19 +0300

    test data corrections

--- generator/group.py
+++ generator/group.py
@@ -24,10 +24,11 @@ for o, a in opts:
         f = a
 
 def random_string(prefix,maxlen):
-    symbols= string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols= string.ascii_letters + string.digits  *10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Group(name="", header="", footer="")] + [
+# testdata = [Group(name="", header="", footer="")] + [
+testdata = [
     Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
     for i in range(n)
     ]
